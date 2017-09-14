677a60f23d2ca8a27188a0c4d0b46f9b0ce1b739
Doug Hagan <doug@stackableregiments.com>
2017-09-14 11:57:15 +1000

    Styling and imports.

--- src/main/scala/metl/comet/Dashboard.scala
+++ src/main/scala/metl/comet/Dashboard.scala
@@ -1,6 +1,5 @@
 package metl.comet
 
-import org.apache.commons.io.IOUtils
 import metl.model._
 import net.liftweb._
 import net.liftweb.actor._
@@ -9,19 +8,12 @@ import http._
 import js._
 import JsCmds._
 import JE._
-import js.jquery.JqJE._
 import json.JsonDSL._
 import json.JsonAST._
-import json.JsonParser._
 import util._
 import Helpers._
 import xml._
-import net.liftweb.http.SHtml._
 import java.util.Date
-import scala.util.Random.shuffle
-import collection.JavaConverters._
-import net.liftweb.common.Logger
-import net.liftweb.util.TimeHelpers
 
 case class StatusCall(id:String,replacementActions:JsCmd, service:String, server:String, serviceCheckMode:ServiceCheckMode)
 
@@ -31,7 +23,6 @@ object CheckAction extends Enumeration {
 }
 
 trait CheckRenderHelper {
-  import GraphableData._
   protected val checkActionJsCmds = Map(
     CheckAction.Create -> "createCheck",
     CheckAction.Destroy -> "destroyCheck",
@@ -39,16 +30,16 @@ trait CheckRenderHelper {
   )
   protected val statusClasses = List("serverOk","serverError","serverUnknown")
   protected def statusClassFromStatus(status:Box[Boolean]):String = {
-    status.map(s => s match {
+    status.map {
       case true => "serverOk"
       case false => "serverError"
-    }).openOr("serverUnknown")
+    }.openOr("serverUnknown")
   }
   protected def statusCodeFromStatus(status:Box[Boolean]):String = {
-    status.map(s => s match {
+    status.map {
       case true => "Y"
       case false => "N"
-    }).openOr("?")
+    }.openOr("?")
   }
   protected def uptimeString(date:Box[Date]):String = date.map(d => d.toString).openOr("NEVER")
   def jsCmdCreator(action:CheckAction.Value, c:CheckResult):JsCmd = Call(checkActionJsCmds(action),jsonForCheckResult(c))
@@ -58,7 +49,7 @@ trait CheckRenderHelper {
     JField("statusCode",JString(statusCodeFromStatus(Full(s.success)))),
     JField("statusClass",statusClasses.map(sc => {
       JField(sc, sc match {
-        case str:String if (str == statusClassFromStatus(Full(s.success))) => JBool(true)
+        case str:String if str == statusClassFromStatus(Full(s.success)) => JBool(true)
         case _ => JBool(false)
       })
     })),
@@ -89,13 +80,13 @@ trait CheckRenderHelper {
       ))
     })))
   ))
-  def jsonForVisualElement(v:VisualElement) = v.jsonRenderer()
-  def jsExpForVisualElement(v:VisualElement) = v.jsExpRenderer()
+  def jsonForVisualElement(v:VisualElement): JObject = v.jsonRenderer()
+  def jsExpForVisualElement(v:VisualElement): JsObj = v.jsExpRenderer()
 }
 
 object DashboardServer extends LiftActor with ListenerManager{
-  def createUpdate = NodeSeq.Empty
-  override def lowPriority = {
+  def createUpdate: NodeSeq = NodeSeq.Empty
+  override def lowPriority: PartialFunction[Any, Unit] = {
     case c:CheckResult => sendListenersMessage(c)
 		case s:StatusCall	=> sendListenersMessage(s)
     case _ => {}
@@ -104,15 +95,15 @@ object DashboardServer extends LiftActor with ListenerManager{
 
 class Dashboard extends CometActor with CometListener with CheckRenderHelper {
   override def lifespan:Box[TimeSpan] = Full(5 minutes)
-  def registerWith = DashboardServer
-  def getJsonStructure = Servers.getVisualElements.map(ve => jsonForVisualElement(ve))
-  def getJsExpStructure = JsObj(Servers.getVisualElements.map(ve => (ve.id,jsExpForVisualElement(ve))):_*)
-  override def render = NodeSeq.Empty
-  override def fixedRender = {
+  def registerWith: DashboardServer.type = DashboardServer
+  def getJsonStructure: List[JObject] = Servers.getVisualElements.map(ve => jsonForVisualElement(ve))
+  def getJsExpStructure: JsObj = JsObj(Servers.getVisualElements.map(ve => (ve.id,jsExpForVisualElement(ve))):_*)
+  override def render: RenderOut = NodeSeq.Empty
+  override def fixedRender: Box[NodeSeq] = {
     "#jsonStructureContainer *" #> Script(JsCrVar("jsonStructure",getJsExpStructure)) 
   }
 
-  override def lowPriority = {
+  override def lowPriority: PartialFunction[Any, Unit] = {
     case s:CheckResult if Globals.currentUserAccessRestriction.permit(s) => partialUpdate(jsCmdCreator(CheckAction.Update,s))
     case _ => {}
   }
